f04a494214e1a704006776549b85da65da76e77d
Sesethu <seshmomoza20@gmail.com>
2022-03-17 12:57:56 +0200

    worked on hide/highlight

--- index.js
+++ index.js
@@ -1,39 +1,30 @@
  const textInputElem = document.querySelector(".textInput");
  const textInputBtn = document.querySelector(".submitBtn");
  const displayElement = document.querySelector(".result");
+ const checkbox = document.querySelector(".checkbox");
+ const checkboxDisplay = document.querySelector(".display");
  let resultsElem = document.querySelector(".results");
  var sentence = "";
   
- 
     function typeSentence(){
 
-    // if (document.querySelector(".textInput").value != null) 
-    // {
-    //   sentence = document.querySelector(".textInput").value;
-    sentence = textInputElem.value;
-    // }
-    // document.querySelector(".result").innerHTML = sentence;
-
+      sentence = textInputElem.value;
     
     function highlightWords() {
    
         let message = ''
         const highlight = sentence.split(" ");
-        console.log(sentence);
        
-        for (let i = 0; i < highlight.length; i++) {
+       for (let i = 0; i < highlight.length; i++) {
             if (highlight[i].length > 4) {
                 
-                message += "<mark>" + highlight[i] +"</mark> "
-                
+             message += "<mark>" + highlight[i] +"</mark> "
             } 
             else {
-                
                 message += highlight[i] + " ";
             }
         }
         sentence = message;
-        console.log({sentence});
     }
     function wordsCounter() {
       
@@ -44,18 +35,37 @@
     // setTimeout(function(){
     //     displayElement.innerHTML = "";
     //     resultsElem.innerHTML = "";
-    // }, 3000);
+    // }, 5000);
 
     return{
         highlightWords,
-        wordsCounter
+        wordsCounter,
+       
     }
  }
     textInputBtn.addEventListener('click',() => { 
     let instance = typeSentence()
     resultsElem.innerHTML = instance.wordsCounter();
     instance.highlightWords();
-    console.log(sentence);
     displayElement.innerHTML = sentence;
 
 });
+function hideSomeWords() {
+    const highlight = sentence.split(" ");
+    let message = '';
+    for (let i = 0; i < highlight.length; i++) {
+        if ((checkbox.checked == true) && (highlight[i].length >= 5)) {
+        
+            message += "<mark>" + highlight[i] +"</mark> "
+
+        } else {
+            message += " ";
+        }
+        console.log(message);
+    }
+   checkboxDisplay.innerHTML = message;
+}
+checkbox.addEventListener('click', function () {
+    hideSomeWords();
+  
+})
